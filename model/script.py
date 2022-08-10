50daa4f51450ebdf032d4a767bc99259c6760619
kanticha954 <kanticha.tri@gmail.com>
2022-08-11 01:10:00 +0700

    model push

--- model/script.py
+++ model/script.py
@@ -1,20 +1,7 @@
-# prediction function
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 12)
-    loaded_model = pickle.load(open("model.pkl", "rb"))
-    result = loaded_model.predict(to_predict)
-    return result[0]
-
-
-@app.route('/result', methods=['POST'])
-def result():
-    if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list)
-        if int(result) == 1:
-            prediction = 'Income more than 50K'
-        else:
-            prediction = 'Income less that 50K'
-        return render_template("result.html", prediction=prediction)
+from azureml.core import Workspace
+ws = Workspace.create(name='modelHand',
+                      subscription_id='9f9160a5-2597-40af-8cef-a374770e6700',
+                      resource_group='handRehab',
+                      create_resource_group=True,
+                      location='East Asia'
+                      )
